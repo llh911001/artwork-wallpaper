67ff19a3873296fceb935d7ad79ef658f4c9edd4
mao <myj226@gmail.com>
2014-11-26 10:53:06 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp');
 
 var coffee = require('gulp-coffee');
-var sass = require('gulp-ruby-sass');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
-  scripts: ['assets/coffee/*.coffee'],
+  clientCoffee: ['assets/coffee/*.coffee'],
     css: ['assets/less/*.less']
 };
 
@@ -15,19 +14,26 @@ gulp.task('less', function () {
   return gulp.src(paths.css)
     .pipe(less())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('stylesheets/css'));
+    .pipe(gulp.dest('public/stylesheets'));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src(paths.scripts)
+gulp.task('client-coffee', function() {
+  return gulp.src(paths.clientCoffee)
     .pipe(coffee({bare: true}))
     // .pipe(uglify())
-    .pipe(gulp.dest('javascripts/'));
+    .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('server-coffee', function() {
+  return gulp.src(['**/*.coffee', '!assets/'])
+    .pipe(coffee({bare: true}))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.scripts, ['client-coffee']);
+  gulp.watch(['**/*.coffee', '!assets/'], ['server-coffee']);
   gulp.watch(paths.css, ['less']);
 });
 
-gulp.task('default', ['scripts', 'less', 'watch']);
+gulp.task('default', ['client-coffee', 'server-coffee', 'less', 'watch']);
