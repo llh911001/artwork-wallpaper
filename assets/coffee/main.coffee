24f575ea7053b2d2c3b7eb3a6f5ff5cbeeb76dcf
mao <myj226@gmail.com>
2014-11-28 13:25:24 +0800

    pure front cannot work

--- assets/coffee/main.coffee
+++ assets/coffee/main.coffee
@@ -4,4 +4,19 @@
   url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json'
   $.get url, (datas)->
     console.log datas
+
+    canvas = document.getElementById("cav")
+    ctx = canvas.getContext("2d")
+
+    imgObj = new Image()
+
+    imgObj.onload = ->
+      ctx.drawImage imgObj, 50, 50
+
+    imgObj.src = datas.topalbums.album[0].image[1]['#text']
+    $('body').append('<button>download</button>')
+    $('button').on 'click', ->
+      canvas.toBlob (blob)->
+          saveAs(blob, "pretty image.png")
+    return
 )()
--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,20 @@
   apiKey = '4dff88a0423651b3570253b10b745b2c';
   url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json';
   return $.get(url, function(datas) {
-    return console.log(datas);
+    var canvas, ctx, imgObj;
+    console.log(datas);
+    canvas = document.getElementById("cav");
+    ctx = canvas.getContext("2d");
+    imgObj = new Image();
+    imgObj.onload = function() {
+      return ctx.drawImage(imgObj, 50, 50);
+    };
+    imgObj.src = datas.topalbums.album[0].image[1]['#text'];
+    $('body').append('<button>download</button>');
+    $('button').on('click', function() {
+      return canvas.toBlob(function(blob) {
+        return saveAs(blob, "pretty image.png");
+      });
+    });
   });
 })();
