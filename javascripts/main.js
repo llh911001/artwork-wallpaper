7c93d5b7b2ee4d942453ed06385f9b6d2b5beee4
Linghao Li <llh911001@gmail.com>
2014-11-30 20:09:10 +0800

    add input to page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,11 @@
-(function() {
+$(function() {
   'use strict';
+
   var apiKey, url;
   apiKey = '4dff88a0423651b3570253b10b745b2c';
-  url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json';
-  return $.get(url, function(datas) {
+  url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&api_key=4dff88a0423651b3570253b10b745b2c&format=json&user=fakelbst';
+
+  return $.getJSON(url, function(datas) {
     var canvas, ctx, imgObj;
     console.log(datas);
     canvas = document.getElementById("cav");
@@ -13,11 +15,10 @@
       return ctx.drawImage(imgObj, 50, 50);
     };
     imgObj.src = datas.topalbums.album[0].image[1]['#text'];
-    $('body').append('<button>download</button>');
-    $('button').on('click', function() {
+    $('#download').on('click', function() {
       return canvas.toBlob(function(blob) {
         return saveAs(blob, "pretty image.png");
       });
     });
   });
-})();
+});
