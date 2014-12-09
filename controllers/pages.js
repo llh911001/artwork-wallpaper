c16c9f7debf6ddd0c5058355671ec1ae57290816
mao <myj226@gmail.com>
2014-12-09 22:13:46 +0800

    update

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var views = require('co-views');
+var request = require('request');
 
 var render = views(__dirname + "/../views", {
-  map: {
-    html: "swig"
-  }
+  map: { html: "swig"}
 });
 
 module.exports.index = function *index(){
-  this.body = yield render("index", {})
+
+    var url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json';
+
+    var img;
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(response)
+        }
+    });
+
+    this.body = yield render("index", {images: img});
 }
+
